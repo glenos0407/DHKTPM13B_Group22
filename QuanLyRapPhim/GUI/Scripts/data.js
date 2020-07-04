efe16a7d19279fc0bf7d6413ecb5f3372d41c329
Chau-Truong-Phat <chautruongphat123@gmail.com>
2020-07-04 13:05:34 +0700

    doing KhachHang UI

---
+++ QuanLyRapPhim/GUI/Scripts/data.js
@@ -0,0 +1,82 @@
+var movies = [
+    {
+        id : "HD001",
+        name : "AVENGERS: END GAME",
+        poster : "../image/poster0.jpg",
+        director: "Anthony Russo, Joe Russo",
+        actor: "Robert Downey JR, Chris Hemsworth, Chris Evans",
+        kind: "Action",
+        premiere: "2019-04-26",
+        time: "182 phút",
+        language: "Phụ đề tiếng Việt",
+        content: "Sau sự kiện Thanos làm cho một nửa vũ trụ tan biến và khiến cho biệt đội Avengers thảm bại, những siêu anh hùng sống sót phải tham gia trận chiến cuối cùng trong Avengers: Endgame - tác phẩm điện ảnh đánh dấu sự khép lại sau 22 bộ phim của Marvel Studios."
+    },
+    {
+        id : "HD002",
+        name : "LẬT MẶT: NHÀ CÓ KHÁCH",
+        poster : "../image/poster1.jpg",
+        director: "Lý Hải",
+        actor: "Huy Khánh, Mạc Văn Khoa",
+        kind: "Horror",
+        premiere: "2019-04-12",
+        time: "100 phút",
+        language: "Phụ đề tiếng Việt",
+        content: "Sự trở lại của đạo diễn Lý Hải sau thành công vang dội của \"Lật Mặt Ba Chàng Khuyết\", điều gì mong chờ ở phía trước cho Katleen Phan Võ, Huy Khánh, Jay Quân, Mạc Văn Khoa,.. và một vài \"vị khách\" không mời?"
+    },
+    {
+        id : "HD003",
+        name : "THE CURSE OF LA LLORONA",
+        poster : "../image/poster2.jpg",
+        director: "Michael Chaves",
+        actor: "Linda Cardellini, Raymond Cruz",
+        kind: "Horror",
+        premiere: "2019-05-03",
+        time: "93 phút",
+        language: "Phụ đề tiếng Việt",
+        content: "Một bom tấn từ ông hoàng phim kinh dị James Wan về hồn ma của ác quỷ tàn khốc La Llorona - người phụ nữ vì ghen tuông mà đã ném hai đứa con của mình xuống sông, sau đó hối hận và trở thành hồn ma, bà đi lang thang khắp nhân gian để tìm lại con của mình."
+    },
+    {
+        id : "HD004",
+        name : "THE QUEEN'S CORGI",
+        poster : "../image/poster3.jpg",
+        director: "Ben Stassen",
+        actor: "Jack Whitehall, Ray Winstone",
+        kind: "Animation",
+        premiere: "2019-04-26",
+        time: "85 phút",
+        language: "Phụ đề tiếng Việt",
+        content: "Câu chuyện về cuộc phưu lưu của Rex, chú chó corgi đáng yêu nhất Hoàng gia Anh. Trong một lần vô tình, Rex bị lưu lạc ra thế giới bên ngoài, nơi mà có rất nhiều thử thách cũng như các nguy hiểm rình rập. Điều gì chờ đợi Rex ở phía trước?."
+    },
+    {
+        id : "HD005",
+        name : "UGLYDOLLS - DUB",
+        poster : "../image/poster4.jpg",
+        director: "Kelly Asbury",
+        actor: "Kelly Clarkson",
+        kind: "Animation",
+        premiere: "2019-05-01",
+        time: "92 phút",
+        language: "Thuyết minh tiếng Việt",
+        content: "Một cuộc phiêu lưu của những chú búp bê lỗi và bị thải ra khỏi nhà máy, liệu khó khăn sẽ được giải quyết thế nào nếu như bạn tồn tại quá khác biệt với định nghĩa về \"xinh đẹp\" của thế giới?"
+    },
+];
+var cinema = [
+    {
+        id : "BHD01",
+        name : "BHD 3/2",
+        address : "Lau 5, Sieu Thi Vincom 3/2,3c Duong 3/2, Quan 10",
+        showtime : ["9:30","10:35","11:20","12:40","13:55","14:40","16:00","17:15"]
+    },
+    {
+        id : "BHD02",
+        name : "BHD Star Bitexco",
+        address : "Lau 3 & 4, TTTM ICON 68, 2 Hai Trieu, Quan 1",
+        showtime : ["9:30","10:35","11:20","12:40","13:55","14:40","16:00","17:15"]
+    },
+    {
+        id : "BHD02",
+        name : "BHD Star Pham Hung",
+        address : "Lau 4, TTTM Satra Pham Hung, C6/27 Pham Hung, Binh Chanh",
+        showtime : ["9:30","10:35","11:20","12:40","13:55","14:40","16:00","17:15"]
+    }
+];
