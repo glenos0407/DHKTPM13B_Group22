2a0f4bbdd82c303066250964a6e5482b15556e78
Chau-Truong-Phat <chautruongphat123@gmail.com>
2020-07-04 14:08:00 +0700

    continue KhachHang_UI

---
+++ QuanLyRapPhim/GUI/KhachHang_UI.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUI
+{
+    public partial class KhachHang_UI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            poster0.Src = "../image/poster0.jpg";
+        }
+    }
+}
